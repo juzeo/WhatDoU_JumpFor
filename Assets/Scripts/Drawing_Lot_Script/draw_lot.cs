ede1b9c3cbc71422bba83d117e079347341bd15b
JuYoung <kaiua1679ing@gmail.com>
2020-11-08 01:01:01 +0900

    scroll 완성 및 상점 선택버튼 구현

---
+++ Assets/Scripts/Drawing_Lot_Script/draw_lot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw_lot : MonoBehaviour
+{
+    public draw_lot instance;
+    public List<Sprite> CharacterList;
+    public List<Sprite> CoinList;
+    public void Start()
+    {
+        instance = this;
+    }
+    public Sprite Start_Draw_lot(string type)
+    {
+        Sprite pickSprite;
+        if (type.Equals("Character"))
+        {
+            pickSprite = CharacterList[UnityEngine.Random.Range(0, CharacterList.Count)];
+        }
+        else if (type.Equals("Coin"))
+        {
+            pickSprite = CoinList[UnityEngine.Random.Range(0, CoinList.Count)];
+        }
+        else
+        {
+            pickSprite = default;
+        }
+
+        return pickSprite;
+    }
+
+    
+}
---
+++ Assets/Scripts/Shop_Script/ShopBtn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBtn : MonoBehaviour
+{
+    public GameObject CharacterScroll;
+    public GameObject CoinScroll;
+    public Image CoinBtnImg;
+    public Image CharacterBtnImg;
+
+    public void CharacterBtn()
+    {
+        CharacterScroll.SetActive(true);
+        Color color;
+        ColorUtility.TryParseHtmlString("#CFCFCF", out color);
+        CharacterBtnImg.color = color;
+        CoinScroll.SetActive(false);
+        ColorUtility.TryParseHtmlString("#FFFFFF", out color);
+        CoinBtnImg.color = color;
+
+    }
+    public void CoinBtn()
+    {
+        CharacterScroll.SetActive(false);
+        Color color;
+        ColorUtility.TryParseHtmlString("#FFFFFF", out color);
+        CharacterBtnImg.color = color;
+        CoinScroll.SetActive(true);
+        ColorUtility.TryParseHtmlString("#CFCFCF", out color);
+
+        CoinBtnImg.color = color;
+    }
+}
