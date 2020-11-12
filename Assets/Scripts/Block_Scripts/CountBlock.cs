d37feec74669f0f2c366eea9d77c5c71f872301d
muida <muida@github.com>
2020-11-13 05:18:03 +0900

    블럭별 스크립트
    2번블럭, 5초후 파괴 블럭
    
    추후 수정

---
+++ Assets/Scripts/Block_Scripts/CountBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountBlock : MonoBehaviour
+{
+    int BlockCount = 2;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("aa");
+        BlockCount--;
+        if (BlockCount==0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
