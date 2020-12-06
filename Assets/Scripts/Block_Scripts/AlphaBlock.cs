2397cc78238c23b0918575bf8d2a912bae72e433
muida <muida@github.com>
2020-12-07 02:18:45 +0900

    스테이지01~05

---
+++ Assets/Scripts/Block_Scripts/AlphaBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaBlock : MonoBehaviour
+{
+    float time;
+
+    void Update()
+    {
+        if (time < 5f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time / 5);
+        }
+        else {
+
+            time = 0;
+            this.gameObject.SetActive(false);
+       
+        }
+
+        time += Time.deltaTime;
+    }
+
+     void Reset()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        this.gameObject.SetActive(true);
+   
+    }
+}
+
